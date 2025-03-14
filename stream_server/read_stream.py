70899b054df89444f27deb77a5e92927be6442fc
Sebastian Bitsch <sebastianbitsch@protonmail.com>
2025-03-14 15:38:56 +0100

    fixed more potential hanging

--- stream_server/read_stream.py
+++ stream_server/read_stream.py
@@ -1,20 +1,26 @@
+import cv2
 import numpy as np
-import cv2 as cv
 
-cap = cv.VideoCapture('http://192.168.2.211:7123/stream.mjpg')
+cap = cv2.VideoCapture('http://192.168.2.211:7123/stream.mjpg')
+
+if not cap.isOpened():
+    cap.release()
+    cv2.destroyAllWindows()
+    print("Failed to open video stream")
+    exit(1)
 
 while cap.isOpened():
     ret, frame = cap.read()
 
-    # if frame is read correctly ret is True
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-    cv.imshow('frame', frame)
-    if cv.waitKey(1) == ord('q'):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) == ord('q'):
         break
 
 cap.release()
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
