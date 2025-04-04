c580a7d1b2e90d0941af801301b3f32223036b43
Sebastian Bitsch <sebastianbitsch@protonmail.com>
2025-04-04 15:49:18 +0200

    fix roundabout bugs, still not good controller

--- mqtt_python/modules/roundabout.py
+++ mqtt_python/modules/roundabout.py
@@ -4,6 +4,7 @@ from sedge import edge
 from spose import pose
 from uservice import service
 import time 
+from math import pi
 
 class Roundabout(Task):
     def __init__(self):
@@ -12,24 +13,25 @@ class Roundabout(Task):
         self.topicRc = service.topicCmd + "ti/rc"
 
         self.turn_start = None
-        self.turn_rate = 0.6 # turn left
-        self.turn_speed = 0.2
         self.move_speed = 0.35 # max 1
 
-        self.Kp = 3 # was 0.5
-        self.Ki = 0
-        self.Kd = 15 # was 0.4
+        # NOTE: Terrible controller
+        self.Kp = 5.0 # 3 # was 0.5
+        self.Ki = 0.0
+        self.Kd = 0.0 # 0.4 # 15 # was 0.4
 
         self.front_wall_detected = False
         self.has_turned_left = False
-        self.target_dist_from_wall = .075
+        self.target_dist_from_wall = 0.2
         self.current_dist_from_wall = None
+        self.cumulative_error = 0.0
+        self.previous_error = 0.0
 
         self.get_to_ramp = False
         self.turn_left = False
         self.has_turned_left_ramp = False
-        self.target_h = 90
-        self.target_h2 = 30
+        self.target_h = pi / 2 # 90 deg
+        self.target_h2 = pi / 6 # 30 deg
         self.h_reset = False
         self.tripB_reset = False
         self.circling = False
@@ -38,7 +40,6 @@ class Roundabout(Task):
         # Avoid extremely small dt
         if dt < 1e-6:
             dt = 1e-6
-
         error = target - current
         self.cumulative_error += error * dt
         # Potentially clamp integrator more tightly or differently
@@ -50,7 +51,8 @@ class Roundabout(Task):
         u = (self.Kp * error) \
             + (self.Ki * self.cumulative_error) \
             + (self.Kd * diff_error)
-        return max(min(u, 1.0), -1.0)
+        print(f"target:{target:.2f}, current:{current:.2f}, e:{error:.2f}, u:{u:.2f}, u1:{max(min(u, 1.0), -1.0):.2f}")
+        return 0.35 * max(min(u, 1.0), -1.0)
 
 
     def follow_wall(self):
@@ -65,11 +67,11 @@ class Roundabout(Task):
         service.send(self.topicRc, command) # send new turn command, maintaining velocity
 
     def loop(self):
-        self.current_dist_from_wall = ir.ir[1]
+        self.current_dist_from_wall = ir.ir[0]
 
         if not self.front_wall_detected and not self.has_turned_left:
             self.follow_wall()
-            if ir.ir[1] < .20:
+            if ir.ir[1] < .30:
                 self.front_wall_detected = True
 
         if self.front_wall_detected and not self.has_turned_left:
@@ -78,12 +80,13 @@ class Roundabout(Task):
                 pose.tripBreset()
                 self.h_reset = True
             service.send(service.topicCmd + "ti/rc", "0.0 0.8") # turn left # speed, angle
+            # print(pose.tripBh)
             if pose.tripBh >= self.target_h:
                 self.has_turned_left = True
 
         if self.front_wall_detected and self.has_turned_left:
             self.follow_wall()
-            if pose.tripB > 1.2:
+            if pose.tripB > 0.4:
                 self.get_to_ramp = True
                 self.back_up = True
 
@@ -93,8 +96,9 @@ class Roundabout(Task):
                 if not self.tripB_reset:
                     self.tripB_reset = True
                     pose.tripBreset()
-                edge.set_line_control_targets(target_velocity = -0.5*self.move_speed, target_position = 0.0)
-                if pose.tripB >= .20:
+                service.send(service.topicCmd + "ti/rc", "0.2 0.0") # turn left # speed, angle
+
+                if pose.tripB >= -0.20:
                     self.back_up = False
                     self.tripB_reset = False
                     self.turn_left = True
