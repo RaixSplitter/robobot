a3199e7d511527aa1ea8cf78fb43911c9f631003
RaixSplitter <markus.bao.quan@gmail.com>
2025-04-15 17:04:23 +0200

    Update navigate_to_pose.py

--- mqtt_python/modules/navigate_to_pose.py
+++ mqtt_python/modules/navigate_to_pose.py
@@ -7,146 +7,210 @@ from uservice import service
 from modules.ball_detection import *
 from modules.aruco import get_pose
 from scam import cam
-import time 
+import time
 import logging
+from enum import Enum
+from dataclasses import dataclass
 
-logging.basicConfig(filename='navigate_to_pose_log',
-                    filemode='a',
-                    format='%(asctime)s,%(msecs)03d %(name)s %(levelname)s %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S',
-                    level=logging.DEBUG)
+logging.basicConfig(
+	filename="navigate_to_pose_log",
+	filemode="a",
+	format="%(asctime)s,%(msecs)03d %(name)s %(levelname)s %(message)s",
+	datefmt="%Y-%m-%d %H:%M:%S",
+	level=logging.DEBUG,
+)
 LOGGER = logging.getLogger(__name__)
-class NavigateToPose(Task):
-	def __init__(self):
-		super().__init__(name='Navigate')
-		self.dont_move = 0.0
-		self.drive_fast = 0.75
-		
-		self.x = None
-		self.y = None
-		self.z = None
-		self.goal_heading = None
-		self.length_to_pose = None
-		self.distances_from_pose = [0.8, 0.35, 0.11]
-		self.distance_from_pose = self.distances_from_pose[0] # meters
-		self.distance_idx = 0
-		self.trip_has_reset = False
-		self.rotate_to_goal_heading = True
-		self.has_turned = False
-		self.drive_straight_to_pose = False
+
+
+class State(Enum):
+	LOOKING_FOR_OBJECT = 'LOOKING FOR OBJECT'
+	TARGET_TURN = 'TARGET TURN'
+	TURN_LEFT = 'TURN LEFT'
+	TURN_RIGHT = 'TURN RIGHT'
+	FORWARD = 'FORWARD'
+	REVERSE = 'REVERSE'
+	CAPTURE = 'CAPTURE'
+class PoseTarget(Enum):
+	ARUCO = 'aruco'
+	BLUE_BALL = 'blue'
+	ORANGE_BALL = 'orange'
+
+@dataclass
+class Target:
+	type : PoseTarget
+	x : float = 0.0
+	y : float = 0.0
+	z : float = 0.0
+	angle: float = 0.0
+	dist : float = 0.0
 	
-	def loop(self, detection_target: str = 'ball'):
-		if not self.trip_has_reset:
-			# LOGGER.debug("Trip is being reset")
-			pose.tripBreset()
-			self.trip_has_reset = True
-			ok, img, imgTime = cam.getImage()
-			# found_poses = pose_est_ball_from_img(img) # (x,y,z) x is left/right, y is up/down, z is inwards/outwards
-			if detection_target == 'aruco':
-				poses_dict = get_pose(img)
-				_, found_pose = list(poses_dict.values())[0]
-				found_poses = [found_pose.flatten()]
-				print("LSMLKASDL", found_poses)
-
-			elif detection_target == 'ball': # Blue
-				found_poses = pose_est_ball_from_img(img, Ball_Color = Ball_Color.BLUE)
-			else:
-				raise ValueError()
-
-			if len(found_poses) == 0: # If no objects found
-				self.trip_has_reset = False
-				print("Error: didnt find any objects in task")
-    
-				#TODO : add a way to retry finding the object
-				return TaskState.FAILURE
-			
-			self.x = found_poses[0][0]
-			self.y = found_poses[0][1]
-			self.z = found_poses[0][2]
-   
-			return TaskState.EXECUTING
-			# print(f"Set goal pose: {self.pose}, x,y,z: {self.x, self.y, self.z}")
-		# To orient yourself with the ball rotate until heading (h) is:
-		# h = arctan(Z/X)
-
-		if self.goal_heading == None and self.length_to_pose == None:
-			LOGGER.debug("Recomputing heading and length to ball")
-			self.goal_heading = np.arctan2(self.x, self.z)
-			self.length_to_pose = np.sqrt(self.x**2+self.z**2)
-			self.has_turned = False
-			self.rotate_to_goal_heading = True
-			print("Heading to goal:", self.goal_heading, "Distance to goal:", self.length_to_pose)
-			pose.tripBreset()
-   
-			return TaskState.EXECUTING
+	def set_pose(self, pose):
+		x, y, z = pose
+		self.x, self.y, self,z = x, y, z
+		self.angle = np.arctan2(x, z)
+		self.dist = np.sqrt(x**2 + z**2)
 		
-		if self.rotate_to_goal_heading and not self.has_turned:
-			LOGGER.debug("Turning to ball position (hopefully)")
-			print("Jeg er her med dig", self.goal_heading)
-			if self.goal_heading < 0: #Turn Left
-				# Turn Left?
-				if -pose.tripBh <= self.goal_heading:
-					print("Done turning left")
-					self.has_turned = True
-					service.send(service.topicCmd + "ti/rc", "0.0 0.0") # stop # speed, angle
-				else:
-					print("turning left")
-					service.send(service.topicCmd + "ti/rc", "0.0 0.1") # turn left # speed, angle
-
-			if self.goal_heading > 0: #Turn Right
-				# Turn Right? 
-				if abs(pose.tripBh) >= self.goal_heading:
-					print("Done turning right")
-					self.has_turned = True
-					service.send(service.topicCmd + "ti/rc", "0.0 0.0") # stop # speed, angle
-				else:
-					print("turning right")
-        
-					service.send(service.topicCmd + "ti/rc", "0.0 -0.1") # turn right # speed, angle
-					
-			if self.has_turned:
-				print("Switching to driving straight")
-				self.drive_straight_to_pose = True
-    
-			return TaskState.EXECUTING
-    
 		
-    
+class NavigateToPose(Task):
+	def __init__(self, target : PoseTarget = PoseTarget.BLUE_BALL):
+		super().__init__(name="Navigate")
+
+		self.SPEED = 0.1
+		self.TURNRATE = 0.1
+		self.ANGLEMARGIN = 0.1
+		self.DISTMARGIN = 0.01
+		self.OFFSET = 0.1 #Offset 11cm
+  
+		self.states_q : list[State] = []
+		self.state: State = State.LOOKING_FOR_OBJECT
+		self.target = Target(target)
+		self.actions = {
+			State.LOOKING_FOR_OBJECT 	: self.get_pose,
+			State.TARGET_TURN 			: self.turn_to_target,
+			State.TURN_LEFT 			: self.turn_left,
+			State.TURN_RIGHT			: self.turn_right,
+			State.FORWARD 				: self.forward,
+			State.REVERSE				: self.reverse,
+			State.CAPTURE 				: self.capture,
+		}
+  
+	def change_state(self) -> None:
+		LOGGER.debug(f"State: {self.state}, State Queue: {self.states_q}")
+		LOGGER.debug(f"Target: {self.target}")
+		
+  
+		if self.states_q: #Get next state
+			self.state = self.states_q.pop(0)
+		else: #Default state
+			self.state = State.LOOKING_FOR_OBJECT
+	
+	def add_state(self, state) -> None:
+		self.states_q.append(state)
+  
+	def stop(self):
+		service.send(service.topCmd + "ti/rc", "0.0 0.0")
+		
+	def turn_to_target(self):
+		pose.tripBreset()
+		if self.target.angle <= -self.ANGLEMARGIN: #If angle is less than margin
+			self.add_state(State.TURN_RIGHT)
+			return False
+		elif self.target.angle >= self.ANGLEMARGIN:#If angle is more than margin
+			self.add_state(State.TURN_LEFT)
+			return False
+		else: #Finished turning
+			return True
+
+	def drive_to_target(self):
+		pose.tripBreset()
+		
+		if self.target.dist - self.OFFSET <= - self.DISTMARGIN:
+			self.add_state(State.)
+   
+
+	def turn_left(self):
+		self.stop()
+		if -pose.tripBh <= self.target.angle: # If done turning
+			self.stop()
+			self.change_state()
+		else:
+			self.turn(direction=True) # Turn right
+	
+	def turn_right(self):
+		self.stop()
+		if pose.tripBh >= self.target.angle: # If done turning
+			self.stop()
+			self.change_state()
+		else:
+			self.turn(direction=False) # Turn right
+
+	
 		
 
-		if self.drive_straight_to_pose:
-			# LOGGER.debug("Driving to ball position")
-			service.send(service.topicCmd + "ti/rc", "0.1 0.0") # drive straight # speed, angle
+	def turn(self, direction : bool = False):
+		"""
+		Turns the robot either left or right
+
+		ARGS:
+			direction : bool, if False turns left, if True turns right.
+		RETURN:
+			None
+  		"""
+		if direction: #Turns right
+			service.send(service.topCmd + "ti/rc", f"0.0 {-self.TURNRATE}")
+		else: # Turns left
+			service.send(service.topCmd + "ti/rc", f"0.0 {self.TURNRATE}")
+	
+	def drive(self, reverse : bool = False):
+		"""
+		Forward or backwards driving
+
+		ARGS:
+			reverse : bool, if False fowards driving, if True reverse driving.
+		RETURN:
+			None
+  		"""
+		if reverse: #Backwards
+			service.send(service.topCmd + "ti/rc", f"{-self.SPEED} 0.0")
+		else: #Forward
+			service.send(service.topCmd + "ti/rc", f"{self.SPEED} 0.0")
    
+	def forward(self):
+     	#Calculate missing distance
+		if pose.tripB >= self.target.dist - self.OFFSET:
+			self.stop()
+			self.change_state()
+		else:
+			self.drive()
+	
+	def reverse(self):
+		#Calculate missing distance
+		if pose.tripB <= self.target.dist - self.OFFSET:
+			self.stop()
+			self.change_state()
+		else:
+			self.drive()
+	
+	def capture(self):
+		service.send(service.topicCmd + "T0/servo", "1, 0 200") # Down position
+		return TaskState.SUCCESS
+	
+	def get_pose(self) -> bool:
+		self.stop()
+  
+		ok, img, imgTime = cam.getImage() # Get image
+  
+		# Get pose
+		if self.target.type == PoseTarget.BLUE_BALL:
+			poses = pose_est_ball_from_img(img, Ball_Color=Ball_Color.BLUE)
+		if len(poses) == 0: #If no poses turn
+			self.turn()
+			return
+		
+		self.target.set_pose(poses[0]) #Set target
 
-			condition_last = self.distance_idx == len(self.distances_from_pose) - 1
-			condition_distance_threshhold = pose.tripB >= self.length_to_pose - self.distance_from_pose
-			
-			print(f"{pose.tripB:.2f}, {self.length_to_pose}, {self.distance_from_pose:.2f}, {self.distance_idx}/{len(self.distances_from_pose)-1}")
-			print(condition_last, condition_distance_threshhold, self.has_turned)
-			print(self.rotate_to_goal_heading, self.has_turned, self.rotate_to_goal_heading and not self.has_turned)
-   
-			if condition_last and condition_distance_threshhold:
-				# LOGGER.debug("Lowering arm to capture ball")
+		#region Validation constraints
+		# VALIDATION STEP 1 TURN
+		if not self.turn_to_target(): #Examine if robot needs to turn towards target
+			self.change_state()
+			return		
 
+		# VALIDATION STEP 2 DRIVE
+		if not self.drive_to_target(): #Examine if robot needs to drive to target or backoff
+			self.change_state()
+			return
+		
+		#endregion
 
-				service.send(service.topicCmd + "ti/rc", "0.0 0.0") # stop # speed, angle
-				self.drive_straight_to_pose = False
-    
-				service.send(service.topicCmd + "T0/servo", "1 0 200") # down position
-				return TaskState.SUCCESS
-   
+		self.state = State.CAPTURE
+		return		
 
-			if condition_distance_threshhold:
-				# LOGGER.debug("Stopping")
-				service.send(service.topicCmd + "ti/rc", "0.0 0.0") # stop # speed, angle
+	def loop(self, detection_target: str = "ball"):
+		action = self.actions[self.state]
+		error = action()
 
-				self.trip_has_reset = False
-				self.distance_idx += 1
-				self.distance_from_pose = self.distances_from_pose[self.distance_idx]
-				self.goal_heading = self.length_to_pose = None
-    
-			return TaskState.EXECUTING
-    
-			
+		if error:
+			return TaskState.FAILURE
+      
 		return TaskState.EXECUTING
+
