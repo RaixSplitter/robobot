e250b5d7bd6ae454234f90fff7aa724aae33997f
Simo333e <simonpontoppidan11@gmail.com>
2025-04-15 10:45:01 +0200

    fixing axe

--- mqtt_python/modules/axe.py
+++ mqtt_python/modules/axe.py
@@ -43,68 +43,68 @@ class Axe(Task):
             if pose.tripB-self.check_point_trip >= 0.4:
                 service.send(service.topicCmd + "ti/rc", "0.0 0.0") # stop # speed, angle
                 self.drive_forward = False
-        
-        if distance < 0.40:
-            if not self.obstacle_active:
-                # We cant go through the axe right now
-                self.obstacle_active = True
-                self.obstacle_start_time = time.time()
+        elif not self.drive_forward:
+            if distance < 0.40:
+                if not self.obstacle_active:
+                    # We cant go through the axe right now
+                    self.obstacle_active = True
+                    self.obstacle_start_time = time.time()
 
-                # When the axe shadows the IR sensor we note the time there was a gap
-                if self.free_active:
-                    self.free_active = False
-                    duration = time.time() - self.free_start_time
-                    self.free_durations.append(duration)
-                    print(f"Free interval: {duration:.2f} s")
+                    # When the axe shadows the IR sensor we note the time there was a gap
+                    if self.free_active:
+                        self.free_active = False
+                        duration = time.time() - self.free_start_time
+                        self.free_durations.append(duration)
+                        print(f"Free interval: {duration:.2f} s")
 
-            if not self.is_running:
-                edge.set_line_control_targets(
-                    target_velocity=self.dont_move,
-                    target_position=0.0
-                )
-                pose.tripBreset()
-                
-
-        
-        else:
-            if not self.free_active:
-                self.free_active = True
-                self.free_start_time = time.time()
-
-                # Once the axe does not shadow the IR sensor we note the time it was free
-                if self.obstacle_active:
-                    self.obstacle_active = False
-                    duration = time.time() - self.obstacle_start_time
-                    self.obstacle_durations.append(duration)
-                    print(f"Obstacle interval: {duration:.2f} s")
+                if not self.is_running:
+                    edge.set_line_control_targets(
+                        target_velocity=self.dont_move,
+                        target_position=0.0
+                    )
+                    pose.tripBreset()
+                    
 
             
-            if not self.is_running and len(self.free_durations) >= 1:
-                self.is_running = True
-                edge.set_line_control_targets(
-                    target_velocity=self.drive_fast,
-                    target_position=0.0
-                )
-                
-        if self.is_running:
-            if pose.tripB >= self.cross_length:
-                self.have_run_through = True
-
-        
-        if self.have_run_through:
-            edge.set_line_control_targets(target_velocity=0.0, target_position=0.0)
-            pose.tripBreset()
-            self.turn_to_face_ball = True
-        
-        if self.turn_to_face_ball:
-            # Turn Right? 
-            if abs(pose.tripBh) >= self.goal_heading:
-                print("Done turning right")
-                self.has_turned = True
-                service.send(service.topicCmd + "ti/rc", "0.0 0.0") # stop # speed, angle
-                return TaskState.SUCCESS
             else:
-                service.send(service.topicCmd + "ti/rc", "0.0 -0.1") # turn right # speed, angle
+                if not self.free_active:
+                    self.free_active = True
+                    self.free_start_time = time.time()
+
+                    # Once the axe does not shadow the IR sensor we note the time it was free
+                    if self.obstacle_active:
+                        self.obstacle_active = False
+                        duration = time.time() - self.obstacle_start_time
+                        self.obstacle_durations.append(duration)
+                        print(f"Obstacle interval: {duration:.2f} s")
+
                 
+                if not self.is_running and len(self.free_durations) >= 1:
+                    self.is_running = True
+                    edge.set_line_control_targets(
+                        target_velocity=self.drive_fast,
+                        target_position=0.0
+                    )
+                    
+            if self.is_running:
+                if pose.tripB >= self.cross_length:
+                    self.have_run_through = True
+
+            
+            if self.have_run_through:
+                edge.set_line_control_targets(target_velocity=0.0, target_position=0.0)
+                pose.tripBreset()
+                self.turn_to_face_ball = True
+            
+            if self.turn_to_face_ball:
+                # Turn Right? 
+                if abs(pose.tripBh) >= self.goal_heading:
+                    print("Done turning right")
+                    self.has_turned = True
+                    service.send(service.topicCmd + "ti/rc", "0.0 0.0") # stop # speed, angle
+                    return TaskState.SUCCESS
+                else:
+                    service.send(service.topicCmd + "ti/rc", "0.0 -0.1") # turn right # speed, angle
+                    
         # time.sleep(0.05)
         return TaskState.EXECUTING
