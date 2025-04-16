955b05670f03c17e8d3010ff61600c963c9545fa
Sunray0466 <son22092000@gmail.com>
2025-04-16 14:16:18 +0200

    Simple roundabout

--- mqtt_python/modules/roundabout_s.py
+++ mqtt_python/modules/roundabout_s.py
@@ -15,16 +15,22 @@ class Roundabout(Task):
         # self.job = self.get_to_pos
         self.job = self.do_a_circle
         
-        # variables
+        ### variables
+        # robot
         self.robot_speed = 0.1
         self.turn_time   = 0.5 # rad
-        self.current_action = [f"{self.robot_speed} 0.0", f"0.0 {self.turn_angle}"]
+        self.Kp = 2.0
+        self.Ki = 0.0
+        self.Kd = 0.4
         
+        # state
+        self.circle_state = 0 # 0 = pre detect, 1 = within range, 2 = out of range
+        self.current_action = [f"{self.robot_speed} 0.0", f"0.0 {self.turn_angle}"]
         self.is_turning     = False
         self.set_turn_time  = 0
+        self.dt             = max(ir.irInterval / 1000.0, 1e-6) # Avoid extremely small dt    
         
-        self.circle_state = 0 # 0 = pre detect, 1 = within range, 2 = out of range
-        
+        # targets
         self.object_dist = (0.2, 0.4) # min/max
         self.drive_dist  = 5 # m?
     
@@ -34,6 +40,21 @@ class Roundabout(Task):
         print("circle state:", self.circle_state, "ir distance:", ir.ir[0], "bh:", pose.tripBh)
         input()
         service.send(service.topicCmd + "ti/rc", self.current_action[0])
+
+    def pid_loop(self, target: float, current: float) -> float:
+        error = target - current
+        self.cumulative_error += error * self.dt
+        # Potentially clamp integrator more tightly or differently
+        self.cumulative_error = max(min(self.cumulative_error, 1.0), -1.0)
+
+        diff_error = (error - self.previous_error) / self.dt
+        self.previous_error = error
+
+        u = (self.Kp * error) \
+          + (self.Ki * self.cumulative_error) \
+          + (self.Kd * diff_error)
+        # print(f"target:{target:.2f}, current:{current:.2f}, e:{error:.2f}, u:{u:.2f}, u1:{max(min(u, 1.0), -1.0):.2f}")
+        return max(min(u, 0.35), -0.35)
 
     def get_to_pos(self):
         # get into position
@@ -77,6 +98,17 @@ class Roundabout(Task):
                 self.circle_state = 0
                 self.set_turn_time = time()
                 self.debug()
+        
+    def do_a_simple_circle(self):
+        if pose.tripB >= self.drive_dist:
+            return TaskState.SUCCESS
+                
+        sensor_d = ir.ir[0]
+        if sensor_d != 1.5:
+            service.send(service.topicCmd + "ti/rc", self.current_action[0]) # drive straight # speed, angle
+        else:
+            service.send(service.topicCmd + "ti/rc", self.current_action[1]) # turning        # speed, angle
+
         
 
     def loop(self):
