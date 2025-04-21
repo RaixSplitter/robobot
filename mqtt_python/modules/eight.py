bbbaa1686afafb970213009fcc86916fe55267e2
Sebastian Bitsch <sebastianbitsch@protonmail.com>
2025-04-21 15:44:21 +0200

    more fixes from mental compiler

--- mqtt_python/modules/eight.py
+++ mqtt_python/modules/eight.py
@@ -11,7 +11,8 @@ class Eight(Task):
         super().__init__(name='eight')
         self.action_start_time = None
         
-        self.toppling_hopper = True
+        self.has_initted = False
+        self.toppling_hopper = False
         self.turning_right = False
         self.driving_into_position = False
         self.found_robot = False
@@ -23,11 +24,15 @@ class Eight(Task):
         self.trip_can_be_reset = True
         
         # 'Parameters'
+        self.pid_values = (0.9, 0.0, 0.5)
+        self.move_speed = 0.25
+        self.line_target = 1.5 # -2...2 where on the line to follow, we want to lean left to hit crossroad
         self.distance_to_hopper = 0.25 # m
         self.right_turn_angle = -3.0 # radians
         self.distance_to_eight = 0.8 + self.distance_to_hopper # m
         self.distances_to_drive_eight = [1.5, 0.9] # m, before and after waiting
         self.wait_times = [3.0, 3.0] # seconds 
+        self.detect_robot_distance = 0.3 # meters
 
     def loop(self):
         """
@@ -40,12 +45,10 @@ class Eight(Task):
         - Wait for robot to pass again
         - Drive quarter of the eight, done        
         """
-        if self.action_start_time is None:
-            self.action_start_time = time.time()
-
-        if self.trip_can_be_reset:
+        if not self.has_initted:
             pose.tripBreset()
-            self.trip_can_be_reset = False
+            self.has_initted = True
+            self.toppling_hopper = True
 
         if self.toppling_hopper:
             # print("Toppling hopper")
@@ -53,12 +56,10 @@ class Eight(Task):
             service.send(service.topicCmd + "ti/rc", "0.3 0.0") # drive forward # speed, angle
 
             if pose.tripB >= self.distance_to_hopper:
-                service.send(service.topicCmd + "T0/servo", "1, -900 200") # Up position
+                service.send(service.topicCmd + "T0/servo", "1, -1000 200") # Up position
                 service.send(service.topicCmd + "ti/rc", "0.0 0.0") # stop # speed, angle
                 self.toppling_hopper = False
                 self.turning_right = True
-                self.action_start_time = time.time()
-                self.trip_can_be_reset = True
 
         if self.turning_right:
             # print("Turning right")
@@ -67,8 +68,7 @@ class Eight(Task):
             if pose.tripBh <= self.right_turn_angle:
                 self.turning_right = False
                 self.driving_into_position = True
-                self.action_start_time = time.time()
-                self.trip_can_be_reset = True
+                pose.tripBreset()
 
         if self.driving_into_position:
             # print("Driving straight")
@@ -77,8 +77,6 @@ class Eight(Task):
             if pose.tripB >= self.distance_to_eight:
                 self.driving_into_position = False
                 self.waiting_for_ir = True
-                self.action_start_time = time.time()
-                self.trip_can_be_reset = True
 
         if self.waiting_for_ir:
             # print("Wating for ir")
@@ -86,31 +84,29 @@ class Eight(Task):
             service.send(service.topicCmd + "ti/rc", "0.0 0.0") # stand still # speed, angle
 
             distance = ir.ir[1]
-            if distance < 0.3 and not self.found_robot:
+            if distance < self.detect_robot_distance and not self.found_robot:
                 # print("self found robot", distance)
                 self.found_robot = True
                 self.action_start_time = time.time()
 
-            if self.found_robot and self.wait_times[0] < time.time() - self.action_start_time:
+            if self.found_robot and self.action_start_time and self.wait_times[0] < time.time() - self.action_start_time:
                 # print("Going")
                 self.waiting_for_ir = False
                 self.found_robot = False
+                self.action_start_time = None
                 self.follow_line = True
-                self.action_start_time = time.time()
                 pose.tripBreset()
 
         if self.follow_line:
             # print("Following line")
-            edge.Kp, edge.Ki, edge.Kd = (0.9, 0.0, 0.5) # TODO
+            edge.Kp, edge.Ki, edge.Kd = self.pid_values
             edge.set_line_control_targets(
-                target_velocity=0.25,
-                target_position=1.5 # follow the left edge of the line to assure we take the right turn in the 8
+                target_velocity=self.move_speed,
+                target_position=self.line_target # follow the left edge of the line to assure we take the right turn in the 8
             )
             if self.distances_to_drive_eight[0] < pose.tripB:
                 self.follow_line = False
                 self.waiting_for_ir_again = True
-                self.action_start_time = time.time()
-                pose.tripBreset()
 
 
         if self.waiting_for_ir_again:
@@ -119,25 +115,25 @@ class Eight(Task):
             service.send(service.topicCmd + "ti/rc", "0.0 0.0") # stand still # speed, angle
 
             distance = ir.ir[1]
-            if distance < 0.3 and not self.found_robot:
+            if distance < self.detect_robot_distance and not self.found_robot:
                 # print("self found robot", distance)
                 self.found_robot = True
                 self.action_start_time = time.time()
 
-            if self.found_robot and self.wait_times[1] < time.time() - self.action_start_time:
+            if self.found_robot and self.action_start_time and self.wait_times[1] < time.time() - self.action_start_time:
                 # print("Going")
                 self.waiting_for_ir_again = False
-                self.found_robot = False
                 self.follow_line_again = True
-                self.action_start_time = time.time()
+                self.found_robot = False
+                self.action_start_time = None
                 pose.tripBreset()
 
         if self.follow_line_again:
             # print("Following line")
-            edge.Kp, edge.Ki, edge.Kd = (0.9, 0.0, 0.5) # TODO
+            edge.Kp, edge.Ki, edge.Kd = self.pid_values
             edge.set_line_control_targets(
-                target_velocity=0.25,
-                target_position=1.5 # follow the left edge of the line to assure we take the right turn in the 8
+                target_velocity=self.move_speed,
+                target_position=self.line_target # follow the left edge of the line to assure we take the right turn in the 8
             )
             if self.distances_to_drive_eight[1] < pose.tripB:
                 return TaskState.SUCCESS
